03b0cff2026dad301e60a4bdb73b25b8e4ca0557
Md. Mehedi Hasan <smmehedi06@gmail.com>
2023-03-13 02:51:17 +0600

    login is tested with dummy data

--- VAT.Infrastructure/Repositories/AccountRepository.cs
+++ VAT.Infrastructure/Repositories/AccountRepository.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using VAT.Domain.Dtos;
@@ -12,7 +15,11 @@ namespace VAT.Infrastructure.Repositories
 	{
 		public Task<string> LogIn(UserDto model)
 		{
-			throw new NotImplementedException();
+			if (model.UserId == "admin" && model.Passwod == "admin")
+			{
+				return Task.FromResult("Login Successful.");
+			}
+			return Task.FromResult(HttpStatusCode.NotFound.ToString());
 		}
 	}
 }
